2c628c4356099181ed4bef28f255b70834a8bdbd
Carlos <carlosedulc17077@gmail.com>
2026-06-08 22:10:07 -0300

    Adicionado alguns presentes na lista

--- src/assets/listadepresentes.ts
+++ src/assets/listadepresentes.ts
@@ -1,4 +1,9 @@
 import qrJantar from './qrcode/jantar_sebastiao.png';
+import pixsemvalor from './qrcode/pix_sem_valor.png';
+import pixdez from './qrcode/pix_dez.png';
+import euchorando from './imagens/eu_chorando.png';
+import razao from './imagens/razao.jpeg';
+
 export const listaPresentes = [
     {
     id: 1,
@@ -8,5 +13,33 @@ export const listaPresentes = [
     imagem: "https://img.freepik.com/fotos-gratis/kitty-com-parede-monocromatica-atras-dela_23-2148955132.jpg",
     qrCode: qrJantar,
     chavePix: "carlosedulc17077@gmail.com" 
-  },
+    },
+    {
+    id: 2,
+    titulo: "Lua de Mel em Ubatuba",
+    descricao: "Uma viagem para a lua de mel em Ubatuba, para aproveitar as praias e a natureza exuberante da região.",
+    valor: "R$ 100,00",
+    imagem: "https://offloadmedia.feverup.com/saopaulosecreto.com/wp-content/uploads/2025/01/03173600/UBATUBA.jpg",
+    qrCode: qrJantar,
+    chavePix: "carlosedulc17077@gmail.com"
+    },
+    {
+    id: 3,
+    titulo: "Lenço para o noivo!",
+    descricao: "Um lenço personalizado para o noivo, para que ele possa enxugar as lágrimas de emoção durante a cerimônia.",
+    valor: "O valor que seu coração mandar",
+    imagem: euchorando,
+    qrCode: pixsemvalor,
+    chavePix: "carlosedulc17077@gmail.com"
+    },
+    {
+    id: 4,
+    titulo: "Cobertor para a noiva estar sempre coberta de razão!",
+    descricao: "Ela sempre tem razão.",
+    valor: "R$ 10,00",
+    imagem: razao,
+    qrCode: pixdez,
+    chavePix: "carlosedulc17077@gmail.com"
+    },
+
   ];
